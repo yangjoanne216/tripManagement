ba688452773ab7aa6cc662f5a53d2b7f2ce0321a
yangyang <yangjoanne216@gmail.com>
2025-06-09 23:59:16 +0200

    init info-service

---
+++ src/test/java/fr/dauphine/miageIf/minh/yang/info_service/InfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dauphine.miageIf.minh.yang.info_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
