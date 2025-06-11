6edfeaac2da0fce7aadbf7a42493b1414fc79e03
yangyang <yangjoanne216@gmail.com>
2025-06-11 11:58:30 +0200

    The basic framework is completed

--- src/main/java/fr/dauphine/miageIf/minh/yang/info_service/mapper/PointOfInterestMapper.java
+++ src/main/java/fr/dauphine/miageIf/minh/yang/info_service/mapper/PointOfInterestMapper.java
@@ -3,9 +3,11 @@ package fr.dauphine.miageIf.minh.yang.info_service.mapper;
 import fr.dauphine.miageIf.minh.yang.info_service.dto.PointOfInterestDto;
 import fr.dauphine.miageIf.minh.yang.info_service.model.PointOfInterest;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface PointOfInterestMapper {
+    PointOfInterestMapper INSTANCE = Mappers.getMapper(PointOfInterestMapper.class);
     PointOfInterestDto toDto(PointOfInterest entity);
     PointOfInterest toEntity(PointOfInterestDto dto);
 }
