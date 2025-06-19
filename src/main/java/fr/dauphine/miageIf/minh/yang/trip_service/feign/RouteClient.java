4e7d14b5e989bd24a60bacb29cf59d2d5fcce7ed
yangyang <yangjoanne216@gmail.com>
2025-06-19 14:58:33 +0200

    change name to connect route-service

--- src/main/java/fr/dauphine/miageIf/minh/yang/trip_service/feign/RouteClient.java
+++ src/main/java/fr/dauphine/miageIf/minh/yang/trip_service/feign/RouteClient.java
@@ -5,7 +5,12 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "ROUTE-SERVICE",path = "")
+
+//@FeignClient(name = "route-service",path ="")
+@FeignClient(
+        name = "route-service",
+        url  = "${route.service.url:http://localhost:8081}"
+)
 public interface RouteClient {
     /**
      * 调用 itinerary-service 的 /itineraries/summary 接口，
