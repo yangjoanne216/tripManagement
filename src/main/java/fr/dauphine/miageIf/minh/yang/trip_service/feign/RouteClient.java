1d0dd75222dc6c129d3d057aef389f80f93e8cd2
yangyang <yangjoanne216@gmail.com>
2025-06-19 11:55:18 +0200

    Clean code, before communicating with the route service

---
+++ src/main/java/fr/dauphine/miageIf/minh/yang/trip_service/feign/RouteClient.java
@@ -0,0 +1,19 @@
+package fr.dauphine.miageIf.minh.yang.trip_service.feign;
+
+import fr.dauphine.miageIf.minh.yang.trip_service.dto.RouteSummaryResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "ROUTE-SERVICE",path = "")
+public interface RouteClient {
+    /**
+     * 调用 itinerary-service 的 /itineraries/summary 接口，
+     * 返回 source 到 destination 的总 distanceKm 和 travelTimeMin
+     */
+    @GetMapping("/itineraries/summary")
+    RouteSummaryResponse getRouteSummary(
+            @RequestParam("source") String sourceCityId,
+            @RequestParam("destination") String destinationCityId
+    );
+}
