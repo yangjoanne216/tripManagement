8fc160d17a13bdc0c1230a45286680c6276fa75b
yangyang <yangjoanne216@gmail.com>
2025-06-22 18:10:53 +0200

    Finish vraiment

--- info-service/src/main/java/fr/dauphine/miageIf/minh/yang/info_service/Feign/RouteServiceClient.java
+++ info-service/src/main/java/fr/dauphine/miageIf/minh/yang/info_service/Feign/RouteServiceClient.java
@@ -4,20 +4,21 @@ import fr.dauphine.miageIf.minh.yang.info_service.dto.RouteCityDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(
+@FeignClient(name = "ROUTE-SERVICE",path ="")
+/*@FeignClient(
         name = "route-service",
         url  = "${route.service.url:http://localhost:8081/cities}"
-)
+)*/
 public interface RouteServiceClient{
-    @PostMapping
+    @PostMapping("/cities")
     RouteCityDto createCity(@RequestBody RouteCityDto city);
 
-    @PutMapping("/{cityId}")
+    @PutMapping("/cities/{cityId}")
     RouteCityDto updateCity(
             @PathVariable("cityId") String cityId,
             @RequestBody RouteCityDto city
     );
 
-    @DeleteMapping("/{cityId}")
+    @DeleteMapping("/cities/{cityId}")
     void deleteCity(@PathVariable("cityId") String cityId);
 }
--- trip-service/src/main/java/fr/dauphine/miageIf/minh/yang/trip_service/feign/RouteClient.java
+++ trip-service/src/main/java/fr/dauphine/miageIf/minh/yang/trip_service/feign/RouteClient.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-//@FeignClient(name = "route-service",path ="")
-@FeignClient(
+@FeignClient(name = "ROUTE-SERVICE",path ="")
+/*@FeignClient(
         name = "route-service",
         url  = "${route.service.url:http://localhost:8081}"
-)
+)*/
 public interface RouteClient {
     /**
      * 调用 itinerary-service 的 /itineraries/summary 接口，
